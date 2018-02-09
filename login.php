2a87affd0d05e59a11669ebc3af3f10950ff873b
Madhumita Sampath <madhumita.sampath@gmail.com>
2018-02-09 18:56:19 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,69 @@
+<?php include('server.php'); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="../../css/style.css" rel="stylesheet">
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Enriqueta" rel="stylesheet">
+    <script src="../../js/react.min.js"></script>
+    <script src="../../js/react-dom.min.js"></script>
+    <script src="../../js/browser.min.js"></script>
+</head>
+<body style="background-color: #2e6da4">
+    <div id="loginpage" style="height: 100%" >
+            <div class="container-fluid decor_bg" id="login-panel">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-1">
+                        <div id="logo">
+                            <h1 style="font-family:Enriqueta; font-size: 128px"><b>connect in.</b></h1>
+                        </div>
+                    </div>
+                    <div class="col-md-5 col-md-offset-1">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4><b>LOGIN</b></h4>
+                            </div>
+                            <div class="panel-body">
+                                <form method="post" action="login.php">
+                                    <?php include('errors.php'); ?>
+                                    <br>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control"  placeholder="Registration Number" name="reg" required = "true">
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" placeholder="Password" name="password" required = "true">
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <button type="submit" name="login_user" class="btn btn-primary">Login</button><br><br>
+                                    </div>
+                                </form><br/>
+                            </div>
+                            <div class="panel-footer"><p>Don't have an account? <a href="register.php">Register</a></p></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <footer>
+            <div id="container">
+                <center>
+                    <p>Copyright &copy; Connect in. All Rights Reserved  |  Contact Us: +91 90000 00000</p> 
+                </center>
+            </div>
+        </footer>
+    <script type="text/babel">
+        ReactDOM.render(
+             document.getElementById('loginpage')
+             document.getElementById('container')
+        );
+
+</body>
+</html>
